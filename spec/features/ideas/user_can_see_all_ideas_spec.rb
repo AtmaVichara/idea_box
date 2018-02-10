fb9c5932cba3161203fa96bd985a8dbadc8f21cd
Joe Jobes <jmrjobes@gmail.com>
2018-02-10 11:19:38 -0700

    Setup idea index feature test

--- spec/features/ideas/user_can_see_all_ideas_spec.rb
+++ spec/features/ideas/user_can_see_all_ideas_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 describe "user sees all ideas" do
   context "from ideas index page" do
     it "shows all ideas" do
+      idea = create_list(:idea, 4)
+      visit ideas_path
+
+      expect(page).to have_content(idea[0].body)
+      expect(page).to have_content(idea[0].title)
+      expect(page).to have_content(idea[1].body)
+      expect(page).to have_content(idea[1].title)
+      expect(page).to have_content(idea[2].body)
+      expect(page).to have_content(idea[2].title)
+      expect(page).to have_content(idea[3].body)
+      expect(page).to have_content(idea[3].title)
     end
   end
 end
