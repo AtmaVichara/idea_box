f5f393e7f7909f423638b01ad254f97875cd26ca
Joe Jobes <jmrjobes@gmail.com>
2018-02-11 07:29:39 -0700

    Add welcome page feature test

---
+++ spec/features/welcomehelper/user_sees_welcome_page_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "user sees welcome page" do
+  context "from root path" do
+    it "show welcome page with links" do
+      visit root_path
+
+      expect(page).to have_link("Sign Up")
+      expect(page).to have_link("Login")
+    end
+  end
+end
