3307f43bb79b8d324e866ffd157809a3fa183bb9
Joe Jobes <jmrjobes@gmail.com>
2018-02-11 07:29:20 -0700

    Add user signup feature test

---
+++ spec/features/users/user_can_create_account_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'user can create account' do
+  context "navigates to signup page from welcome screen" do
+    it 'creates an account' do
+      visit root_path
+
+      click_on "Sign Up"
+
+      fill_in "user[username]", with: 'AtmaVicharaEsq'
+      fill_in "user[email]", with: "atma_vichara_jewel@gmail.com"
+      fill_in "user[password_digest]", with: "Linked through the net of Indra"
+      click_on "Submit"
+
+      expect(current_path).to eq('/users/1')
+      expect(page).to have_content("AtmaVicharaEsq")
+    end
+  end
+end
