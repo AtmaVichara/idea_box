3400f5993652d7e8831260366ee05014306721f4
Joe Jobes <jmrjobes@gmail.com>
2018-02-11 08:21:28 -0700

    Add new view for sessions

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe "user can login" do
   context "navigates to login page from welcome page" do
     it "logins in user" do
-      user = User.create!(gusername: "AtmaVichara", email: "AtmaV@gmail.com", password_digest: "password")
+      user = User.create!(username: "AtmaVichara", email: "AtmaV@gmail.com", password_digest: "password")
       visit root_path
 
       click_on "Login"
 
-      fill_in "session[email]", with: "AtmaV@gmail.com"
-      fill_in "session[password]", with: "password"
+      fill_in "email", with: "AtmaV@gmail.com"
+      fill_in "password", with: "password"
       click_on "Login"
 
       expect(page).to eq(user_path(user))
