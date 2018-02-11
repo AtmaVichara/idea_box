ca9933a16667797a0f7fdc8b47a4a9e368baac29
Joe Jobes <jmrjobes@gmail.com>
2018-02-11 10:59:02 -0700

    Add show feature test for category

---
+++ spec/features/categories/user_sees_single_category_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "user sees single category" do
+  context "navigate from index page" do
+    it "shows single category" do
+      category = create(:category)
+      idea = create(:idea, category_id: category)
+      idea2 = create(:idea, category_id: category)
+      idea3 = create(:idea, category_id: category)
+
+      visit categories_path
+
+      click_link "#{category.name}"
+
+      expect(page).to have_link(idea.title)
+      expect(page).to have_link(idea2.title)
+      expect(page).to have_link(idea3.title)
+    end
+  end
+end
