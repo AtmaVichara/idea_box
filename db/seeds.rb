6b0a460954ed2db38223ca1c3329b0014889c843
Joe Jobes <jmrjobes@gmail.com>
2018-02-12 07:43:02 -0700

    Add seeds for user and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,27 @@
 require 'csv'
 
 Idea.destroy_all
+User.destroy_all
+Category.destroy_all
 
 USERS = ['AtmaVichara', 'Dangodiply', 'Pinkrando', 'Megamess']
 EMAILS = ['rando@gmail.com', 'someother@hotmail.com', 'gipsycrypto@yahoo.com', 'authcash@gmail.com']
-PASSWORDS = ['password', '123456789']
+CATEGORIES = ['World Domination', 'Grand Mistakes', 'Epiphanies', 'Childish Banter']
 
+
+USERS.each_with_index do |user, index|
+  User.create!(username: user, email: EMAILS[index], password: 'password')
+end
+
+CATEGORIES.each do |cat|
+  Category.create!(name: cat)
+end
 
 CSV.foreach("./data/ideas.csv", headers: true) do |row|
-  Idea.create!(title: row[0], body: row[1])
+  Idea.create!(
+    title: row[0],
+    body: row[1],
+    category_id: Category.all.sample.id,
+    user_id: User.all.sample.id
+  )
 end
