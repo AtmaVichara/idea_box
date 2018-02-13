f1f44422927f2e648baa7eca6768cc77260c692a
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 13:28:28 -0700

    Add edge case test for regular user trying to enter admin category show page

--- spec/features/categories/user_sees_single_category_spec.rb
+++ spec/features/categories/user_sees_single_category_spec.rb
@@ -18,4 +18,16 @@ describe "user sees single category" do
       expect(page).to have_link(idea3.title)
     end
   end
+  context 'from admin category show page' do
+    it 'shows 404 page' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      category = create(:category)
+
+      visit admin_category_path(category)
+
+      expect(page).to_not have_link("Edit Category")
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
 end
