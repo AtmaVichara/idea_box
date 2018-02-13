60e7618028262b1da78ae91058cb18c701f5720b
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 14:34:27 -0700

    Add validations to image spec for content type

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,4 +5,8 @@ describe Image, type: :model do
     it {should have_many(:idea_images)}
     it {should have_many(:ideas).through(:idea_images)}
   end
+  describe "validations" do
+    it {should validate_attachment_content_type(:image).
+                      allowing("image/jpg", "image/jpeg", "image/png")}
+  end
 end
