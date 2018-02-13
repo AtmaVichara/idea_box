630c4464edff507eed6671390ef7da03acd36257
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 16:46:58 -0700

    Add images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,15 @@
 require 'csv'
 
 Idea.destroy_all
+Image.destroy_all
 User.destroy_all
 Category.destroy_all
 
+IMAGES = ["http://www.freepngimg.com/download/red_panda/8-2-red-panda-transparent.png",
+          "http://img1.ak.crunchyroll.com/i/spire3/3614810e9ada5235038e8deb4adc264c1447729591_large.jpg",
+          "https://joaomfjorge.files.wordpress.com/2015/06/philosophy-icon.png",
+          "https://www.spreadshirt.com/image-server/v1/mp/designs/13409433,width=178,height=178/weightlifter-weightlifting-woman.png",
+          "https://d30y9cdsu7xlg0.cloudfront.net/png/17148-200.png"]
 USERS = ['AtmaVichara', 'Dangodiply', 'Pinkrando', 'Megamess']
 EMAILS = ['rando@gmail.com', 'someother@hotmail.com', 'gipsycrypto@yahoo.com', 'authcash@gmail.com']
 CATEGORIES = ['World Domination', 'Grand Mistakes', 'Epiphanies', 'Childish Banter']
@@ -31,4 +37,8 @@ CSV.foreach("./data/ideas.csv", headers: true) do |row|
     category_id: Category.all.sample.id,
     user_id: User.all.sample.id
   )
+end
+
+IMAGES.each do |image|
+  Image.create!(image_url: image)
 end
