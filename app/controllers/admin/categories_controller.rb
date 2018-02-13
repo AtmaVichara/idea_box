9f9dd721234de06480370dce09eda53301c25138
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 13:07:53 -0700

    Add show action to admin::categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,11 @@
 class Admin::CategoriesController < Admin::BaseController
-  before_action :set_category, only: [:destroy]
+  before_action :set_category, only: [:destroy, :show]
 
   def index
     @categories = Category.all
+  end
+
+  def show
   end
 
   def new
