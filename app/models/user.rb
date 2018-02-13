2090a81268369ea88238fefec24d4dc630504dec
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 11:33:37 -0700

    Add enum to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :username, :email, :password_digest
-  has_secure_password
   has_many :ideas
+  has_secure_password
+
+  enum role: ['default', 'admin']
 end
