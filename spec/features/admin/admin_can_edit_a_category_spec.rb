e20e788d2bea31e508d5b5e5bf531166270febc1
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 13:06:26 -0700

    Add admin category edit feature test

---
+++ spec/features/admin/admin_can_edit_a_category_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "admin can edit category" do
+  context "from the admin category show page" do
+    it "edits category" do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      category = create(:category)
+
+      visit admin_category_path(category)
+
+      click_on "Edit"
+
+      fill_in 'category[name]', with: "World Changers"
+      click_on "Edit Category"
+
+      expect(current_path).to eq(admin_category_path(category))
+      expect(page).to have_content("World Changers")
+    end
+  end
+end
