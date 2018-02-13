503cdbd6a2f8330c4fca0cd2838cffcd053a70f1
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 11:23:18 -0700

    Add admin test for creating categories

--- spec/features/admin/admin_can_create_category_spec.rb
+++ spec/features/admin/admin_can_create_category_spec.rb
@@ -1,8 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe "admin can create categories" do
   context "from category index page" do
     it "creates new category" do
+      admin = factory(:admin)
+      
       visit categories_path
 
       click_on "Create Category"
