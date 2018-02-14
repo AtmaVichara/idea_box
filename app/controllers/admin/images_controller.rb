b89834a29f5d6a9804b7cb26ae3e0326dbb260de
Joe Jobes <jmrjobes@gmail.com>
2018-02-13 17:08:16 -0700

    Add create action for admin images

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -4,4 +4,23 @@ class Admin::ImagesController < Admin::BaseController
     @images = Image.all
   end
 
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to admin_images_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def image_params
+      params.require(:image).permit(:image_url)
+    end
+
 end
