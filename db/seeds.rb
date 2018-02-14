7e52b011240911a548a4fb3accd64a48faafb6c6
Joe Jobes <jmrjobes@gmail.com>
2018-02-14 12:41:41 -0700

    Add image names to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ Image.destroy_all
 User.destroy_all
 Category.destroy_all
 
-IMAGES = ["http://www.freepngimg.com/download/red_panda/8-2-red-panda-transparent.png",
-          "http://img1.ak.crunchyroll.com/i/spire3/3614810e9ada5235038e8deb4adc264c1447729591_large.jpg",
-          "https://joaomfjorge.files.wordpress.com/2015/06/philosophy-icon.png",
-          "https://www.spreadshirt.com/image-server/v1/mp/designs/13409433,width=178,height=178/weightlifter-weightlifting-woman.png",
-          "https://d30y9cdsu7xlg0.cloudfront.net/png/17148-200.png"]
+IMAGES = [["red panda", "http://www.freepngimg.com/download/red_panda/8-2-red-panda-transparent.png"],
+          ["one punch man", "http://img1.ak.crunchyroll.com/i/spire3/3614810e9ada5235038e8deb4adc264c1447729591_large.jpg"],
+          ["socrates", "https://joaomfjorge.files.wordpress.com/2015/06/philosophy-icon.png"],
+          ["weightlifting", "https://www.spreadshirt.com/image-server/v1/mp/designs/13409433,width=178,height=178/weightlifter-weightlifting-woman.png"],
+          ["meditation", "https://d30y9cdsu7xlg0.cloudfront.net/png/17148-200.png"]]
 USERS = ['AtmaVichara', 'Dangodiply', 'Pinkrando', 'Megamess']
 EMAILS = ['rando@gmail.com', 'someother@hotmail.com', 'gipsycrypto@yahoo.com', 'authcash@gmail.com']
 CATEGORIES = ['World Domination', 'Grand Mistakes', 'Epiphanies', 'Childish Banter']
@@ -40,5 +40,5 @@ CSV.foreach("./data/ideas.csv", headers: true) do |row|
 end
 
 IMAGES.each do |image|
-  Image.create!(image_url: image)
+  Image.create!(image_url: image[1], name: [0])
 end
